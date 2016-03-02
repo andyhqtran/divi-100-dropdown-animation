3482874aa36ff606fbbb1b61448d31a2f92b4e32
Fikri Rasyid <fikrirasyid@gmail.com>
2016-03-02 14:22:08 +0700

    Loaded Divi 100 after all Divi 100 plugins have been registered

--- dropdown-animation.php
+++ dropdown-animation.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
-* @package Custom_Dropdown_Animations
+* @package Custom_Dropdown_Animation
 * @version 0.0.1
 */
 
@@ -16,22 +16,88 @@
 */
 
 /**
- * Load Divi 100 Setup
+ * Register plugin to Divi 100 list
  */
-require_once( plugin_dir_path( __FILE__ ) . '/divi-100-setup/divi-100-setup.php' );
+class ET_Divi_100_Custom_Dropdown_Animation_Config {
+	public static $instance;
+
+	/**
+	 * Hook the plugin info into Divi 100 list
+	 */
+	function __construct() {
+		add_filter( 'et_divi_100_settings', array( $this, 'register' ) );
+		add_action( 'plugins_loaded',       array( $this, 'init' ) );
+	}
+
+	/**
+	* Gets the instance of the plugin
+	*/
+	public static function instance(){
+		if ( null === self::$instance ){
+			self::$instance = new self();
+		}
+
+		return self::$instance;
+	}
+
+	/**
+	 * Define plugin info
+	 *
+	 * @return array plugin info
+	 */
+	public static function info() {
+		$main_prefix = 'et_divi_100_';
+		$plugin_slug = 'custom_dropdown_animations';
+
+		return array(
+			'main_prefix'        => $main_prefix,
+			'plugin_name'        => __( 'Custom Dropdown Animation' ),
+			'plugin_description' => __( 'Nullam quis risus eget urna mollis ornare vel eu leo.' ),
+			'plugin_slug'        => $plugin_slug,
+			'plugin_id'          => "{$main_prefix}{$plugin_slug}",
+			'plugin_prefix'      => "{$main_prefix}{$plugin_slug}-",
+			'plugin_version'     => 20160302,
+			'plugin_dir_path'    => plugin_dir_path( __FILE__ ),
+		);
+	}
+
+	/**
+	 * et_divi_100_settings callback
+	 *
+	 * @param array  settings
+	 * @return array settings
+	 */
+	function register( $settings ) {
+		$info = self::info();
+
+		$settings[ $info['plugin_slug'] ] = $info;
+
+		return $settings;
+	}
+
+	/**
+	 * Init plugin after all plugins has been loaded
+	 */
+	function init() {
+		// Load Divi 100 Setup
+		require_once( plugin_dir_path( __FILE__ ) . 'divi-100-setup/divi-100-setup.php' );
+
+		// Load Dropdown Animation
+		ET_Divi_100_Custom_Dropdown_Animation::instance();
+	}
+}
+ET_Divi_100_Custom_Dropdown_Animation_Config::instance();
 
 /**
- * Load Custom Dropdown Animations
+ * Load Custom Dropdown Animation
+ * this requires Divi 100 setup to be loaded first
  */
-class ET_Divi_100_Custom_Dropdown_Animations {
+class ET_Divi_100_Custom_Dropdown_Animation {
 	/**
 	* Unique instance of plugin
 	*/
 	public static $instance;
-	public $main_prefix;
-	public $plugin_slug;
-	public $plugin_id;
-	public $plugin_prefix;
+	public $config;
 	protected $settings;
 	protected $utils;
 
@@ -50,12 +116,9 @@ class ET_Divi_100_Custom_Dropdown_Animations {
 	* Constructor
 	*/
 	private function __construct(){
-		$this->main_prefix   = 'et_divi_100_';
-		$this->plugin_slug   = 'custom_dropdown_animations';
-		$this->plugin_id     = "{$this->main_prefix}{$this->plugin_slug}";
-		$this->plugin_prefix = "{$this->plugin_id}-";
-		$this->settings      = maybe_unserialize( get_option( $this->plugin_id ) );
-		$this->utils         = new Divi_100_Utils( $this->settings );
+		$this->config   = ET_Divi_100_Custom_Dropdown_Animation_Config::info();
+		$this->settings = maybe_unserialize( get_option( $this->config['plugin_id'] ) );
+		$this->utils    = new Divi_100_Utils( $this->settings );
 
 		// Initialize if Divi is active
 		if ( et_divi_100_is_active() ) {
@@ -69,35 +132,35 @@ class ET_Divi_100_Custom_Dropdown_Animations {
 	* @return void
 	*/
 	private function init(){
-		add_action( 'wp_enqueue_scripts',    array( $this, 'enqueue_frontend_scripts' ) );
-		add_filter( 'body_class',            array( $this, 'body_class' ) );
+		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_frontend_scripts' ) );
+		add_filter( 'body_class',         array( $this, 'body_class' ) );
 
 		if ( is_admin() ) {
 			$settings_args = array(
-				'plugin_id'       => $this->plugin_id,
+				'plugin_id'       => $this->config['plugin_id'],
 				'preview_dir_url' => plugin_dir_url( __FILE__ ) . 'preview/',
-				'title'           => __( 'Custom Dropdown Animations' ),
-				'description'     => __( 'Nullam quis risus eget urna mollis ornare vel eu leo.' ),
+				'title'           => $this->config['plugin_name'],
+				'description'     => $this->config['plugin_description'],
 				'fields' => array(
 					array(
-						'type'                 => 'select',
-						'preview_prefix'       => 'style-',
-						'preview_height'       => 300,
-						'id'                   => 'primary-style',
-						'label'                => __( 'Primary Nav Style' ),
-						'description'          => __( 'Proper description goes here' ),
-						'options'              => $this->get_primary_styles(),
-						'sanitize_callback'    => 'sanitize_text_field',
+						'type'              => 'select',
+						'preview_prefix'    => 'style-',
+						'preview_height'    => 300,
+						'id'                => 'primary-style',
+						'label'             => __( 'Primary Nav Style' ),
+						'description'       => __( 'Proper description goes here' ),
+						'options'           => $this->get_primary_styles(),
+						'sanitize_callback' => 'sanitize_text_field',
 					),
 					array(
-						'type'                 => 'select',
-						'preview_prefix'       => 'style-',
-						'preview_height'       => 300,
-						'id'                   => 'secondary-style',
-						'label'                => __( 'Secondary Nav Style' ),
-						'description'          => __( 'Proper description goes here' ),
-						'options'              => $this->get_secondary_styles(),
-						'sanitize_callback'    => 'sanitize_text_field',
+						'type'              => 'select',
+						'preview_prefix'    => 'style-',
+						'preview_height'    => 300,
+						'id'                => 'secondary-style',
+						'label'             => __( 'Secondary Nav Style' ),
+						'description'       => __( 'Proper description goes here' ),
+						'options'           => $this->get_secondary_styles(),
+						'sanitize_callback' => 'sanitize_text_field',
 					),
 				),
 				'button_save_text' => __( 'Save Changes' ),
@@ -113,7 +176,7 @@ class ET_Divi_100_Custom_Dropdown_Animations {
 	* @return array
 	*/
 	function get_primary_styles() {
-		return apply_filters( $this->plugin_prefix . 'primary_styles', array(
+		return apply_filters( $this->config['plugin_prefix'] . 'primary_styles', array(
 			''             => __( 'Default' ),
 			'fadeIn'       => __( 'Fade In' ),
 			'fadeInTop'    => __( 'Fade In From Top' ),
@@ -134,7 +197,7 @@ class ET_Divi_100_Custom_Dropdown_Animations {
 	* @return array
 	*/
 	function get_secondary_styles() {
-		return apply_filters( $this->plugin_prefix . 'secondary_styles', array(
+		return apply_filters( $this->config['plugin_prefix'] . 'secondary_styles', array(
 			''             => __( 'Default' ),
 			'fadeIn'       => __( 'Fade In' ),
 			'fadeInTop'    => __( 'Fade In From Top' ),
@@ -160,12 +223,12 @@ class ET_Divi_100_Custom_Dropdown_Animations {
 		$selected_secondary_style = $this->utils->get_value( 'secondary-style' );
 
 		if ( '' !== $selected_primary_style ) {
-			$classes[] = esc_attr(  $this->plugin_prefix . '-primary' );
+			$classes[] = esc_attr(  $this->config['plugin_prefix'] . '-primary' );
 			$classes[] = esc_attr( "et_primary_nav_dropdown_animation_{$selected_primary_style}" );
 		}
 
 		if ( '' !== $selected_secondary_style ) {
-			$classes[] = esc_attr(  $this->plugin_prefix . '-secondary' );
+			$classes[] = esc_attr(  $this->config['plugin_prefix'] . '-secondary' );
 			$classes[] = esc_attr( "et_secondary_nav_dropdown_animation_{$selected_secondary_style}" );
 		}
 
@@ -178,8 +241,7 @@ class ET_Divi_100_Custom_Dropdown_Animations {
 	* @return void
 	*/
 	function enqueue_frontend_scripts() {
-		wp_enqueue_style( 'custom-dropdown-animations', plugin_dir_url( __FILE__ ) . 'css/style.css' );
-		wp_enqueue_script( 'custom-dropdown-animations', plugin_dir_url( __FILE__ ) . 'js/scripts.js', array( 'jquery', 'divi-custom-script' ), '0.0.1', true );
+		wp_enqueue_style( 'custom-dropdown-animations', plugin_dir_url( __FILE__ ) . 'css/style.css', array(), $this->config['plugin_version'] );
+		wp_enqueue_script( 'custom-dropdown-animations', plugin_dir_url( __FILE__ ) . 'js/scripts.js', array( 'jquery', 'divi-custom-script' ), $this->config['plugin_version'], true );
 	}
 }
-ET_Divi_100_Custom_Dropdown_Animations::instance();
